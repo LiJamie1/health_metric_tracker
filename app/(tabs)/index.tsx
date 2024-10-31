7029882656b1872f3c7770abf1c9a37df1704a5c
Jamie Li <lijamie19828@gmail.com>
2024-10-31 13:40:42 -0700

    google signin now returns response object, signin and signout buttons work, add button test to check state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,43 @@
 import { useEffect, useState } from 'react';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from 'src/components/ThemedText';
+import { ThemedView } from 'src/components/ThemedView';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { Button } from 'react-native';
 import {
   GoogleSignin,
   GoogleSigninButton,
-  statusCodes,
 } from '@react-native-google-signin/google-signin';
 import axios from 'axios';
-import Constants from 'expo-constants';
-import styles from '@/constants/Styling';
+import styles from 'src/constants/Styling';
+
+interface User {
+  email: string;
+  familyName: string | null;
+  givenName: string | null;
+  id: string;
+  name: string | null;
+  photo: string | null;
+}
+interface Data {
+  idToken: string | null;
+  scopes: string[];
+  serverAuthCode: string | null;
+  user: User | null;
+}
+
+interface GoogleResponse {
+  data: Data | null;
+  type: string;
+}
 
 export default function Index() {
   // Get Local network ip to connect via expo go app if not default to localhost
-  const LOCAL_NETWORK_IP =
-    Constants?.expoConfig?.extra?.localNetworkIp;
-
-  const localHost = LOCAL_NETWORK_IP
-    ? LOCAL_NETWORK_IP
-    : 'http://localhost:5000/';
-
-  const GOOGLE_CLIENT_ID =
-    Constants?.expoConfig?.extra?.googleClientId;
+  // const localHost = LOCAL_NETWORK_IP ? LOCAL_NETWORK_IP : 'http://localhost:5000/';
 
   const configureGoogleSignIn = () => {
     GoogleSignin.configure({
-      webClientId: GOOGLE_CLIENT_ID,
+      webClientId:
+        '270383151259-tb94eleqlvhm3lt4i268qdu9re98a709.apps.googleusercontent.com',
     });
   };
 
@@ -34,41 +45,53 @@ export default function Index() {
     configureGoogleSignIn();
   });
 
+  const emptyUserInfo = {
+    data: null,
+    type: 'signedOut',
+  };
+
   const [error, setError] = useState<string | null>(null);
-  const [userInfo, setUserInfo] = useState<unknown>();
+  const [userInfo, setUserInfo] =
+    useState<GoogleResponse>(emptyUserInfo);
 
   const signIn = async () => {
     console.log('Pressed Sign In');
 
     try {
       await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
-      setUserInfo(userInfo);
+      const resUserInfo = await GoogleSignin.signIn();
+      setUserInfo(resUserInfo);
       setError(null);
       console.log('Sign in Successful');
     } catch (e: unknown) {
       setError(e instanceof Error ? e.message : 'An error occurred');
+      console.log(error);
     }
   };
 
   const logout = () => {
-    setUserInfo(undefined);
+    setUserInfo(emptyUserInfo);
     GoogleSignin.revokeAccess();
     GoogleSignin.signOut();
+    console.log('Sign Out Successful');
   };
 
   // Get Axios Response from "/" endpoint declared in backend/src/index.ts
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`${localHost}`);
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get(`${localHost}`);
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
 
-  const testLocalHost = () => {
-    console.log(localHost);
+  // const testLocalHost = () => {
+  //   console.log(localHost);
+  // };
+
+  const consoleUserInfo = () => {
+    console.log(userInfo.type);
   };
 
   return (
@@ -77,8 +100,14 @@ export default function Index() {
         <ThemedText type="title">Index</ThemedText>
       </ThemedView>
       <ThemedView style={styles.content}>
-        <ThemedText type="default">{error}</ThemedText>
-        {userInfo ? (
+        {userInfo.type === 'success' ? (
+          <ThemedText type="default">
+            Hello {userInfo.data?.user?.name}
+          </ThemedText>
+        ) : (
+          <ThemedText type="default">{error}</ThemedText>
+        )}
+        {userInfo.type === 'success' ? (
           <Button title="Logout" onPress={logout} />
         ) : (
           <GoogleSigninButton
@@ -90,8 +119,9 @@ export default function Index() {
         <ExpoStatusBar style="auto" />
       </ThemedView>
       <ThemedView style={styles.content}>
-        <Button title="Local Host Url Test" onPress={testLocalHost} />
-        <Button title="Backend Response Test" onPress={fetchData} />
+        {/* <Button title="Local Host Url Test" onPress={testLocalHost} />
+        <Button title="Backend Response Test" onPress={fetchData} /> */}
+        <Button title="Check userInfo" onPress={consoleUserInfo} />
       </ThemedView>
     </ThemedView>
   );
