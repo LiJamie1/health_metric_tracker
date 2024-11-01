77e5dbce495108abb84d0dcdde9bed073d958bf6
Jamie Li <lijamie19828@gmail.com>
2024-10-31 17:58:17 -0700

    undo removal of react native google sign in code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,73 @@
+import { useEffect, useState } from 'react';
 import { ThemedText } from 'src/components/ThemedText';
 import { ThemedView } from 'src/components/ThemedView';
+import { GoogleResponse } from 'src/interfaces/GoogleResponse';
 import styles from 'src/constants/Styling';
 import { Button } from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
 import axios from 'axios';
 
 export default function Index() {
-  // Get Local network ip to connect via expo go app if not default to localhost
   const localHost =
     'https://2329-2604-3d08-517d-c600-18aa-1995-6c79-59fe.ngrok-free.app';
 
-  // Get Axios Response from "/" endpoint declared in backend/src/index.ts
+  const configureGoogleSignIn = () => {
+    GoogleSignin.configure({
+      webClientId:
+        '270383151259-tb94eleqlvhm3lt4i268qdu9re98a709.apps.googleusercontent.com',
+      scopes: [
+        'https://www.googleapis.com/auth/spreadsheets.readonly',
+      ],
+      offlineAccess: true,
+      forceCodeForRefreshToken: true,
+    });
+  };
+
+  useEffect(() => {
+    configureGoogleSignIn();
+  });
+
+  const emptyUserInfo = {
+    data: null,
+    type: 'signedOut',
+  };
+
+  const [error, setError] = useState<string | null>(null);
+  // Remove state for userInfo
+  // Currently no reason to hold on to this
+  const [userInfo, setUserInfo] =
+    useState<GoogleResponse>(emptyUserInfo);
+
+  const signIn = async () => {
+    console.log('Pressed Sign In');
+
+    try {
+      await GoogleSignin.hasPlayServices();
+      const resUserInfo = await GoogleSignin.signIn();
+      // Add if with isSuccessResponse
+      // https://react-native-google-signin.github.io/docs/api#issuccessresponse
+      // Just send off serverAuthCode to backend via ${localHost}/api/auth/google
+      // Wait for a response from the backend that we got a token, no need to send the token
+      setUserInfo(resUserInfo);
+      setError(null);
+      console.log('Sign in Successful');
+    } catch (e: unknown) {
+      setError(e instanceof Error ? e.message : 'An error occurred');
+      console.log(error);
+    }
+  };
+
+  const logout = () => {
+    setUserInfo(emptyUserInfo);
+    GoogleSignin.revokeAccess();
+    GoogleSignin.signOut();
+    console.log('Sign Out Successful');
+  };
+
+  // Test Axios Response from "/" endpoint declared in backend/src/index.ts
   const fetchData = async () => {
     try {
       const response = await axios.get(`${localHost}`);
@@ -19,15 +77,33 @@ export default function Index() {
     }
   };
 
-  const testLocalHost = () => {
-    console.log(localHost);
+  // Check userInfo state stored
+  // Will be removed as I don't need to hold on to user info for now
+  const consoleUserInfo = () => {
+    console.log(userInfo);
   };
 
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">Home</ThemedText>
       <ThemedView style={styles.content}>
-        <Button title="Local Host Url Test" onPress={testLocalHost} />
+        {userInfo.type === 'success' ? (
+          <ThemedText type="default">
+            Hello {userInfo.data?.user?.name}
+          </ThemedText>
+        ) : (
+          <ThemedText type="default">{error}</ThemedText>
+        )}
+        {userInfo.type === 'success' ? (
+          <Button title="Logout" onPress={logout} />
+        ) : (
+          <GoogleSigninButton
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={signIn}
+          />
+        )}
+      </ThemedView>
+      <ThemedView style={styles.content}>
         <Button title="Backend Response Test" onPress={fetchData} />
       </ThemedView>
     </ThemedView>
