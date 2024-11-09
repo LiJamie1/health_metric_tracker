beecc400047098805b33c60e80fc5b51c0687fa7
Jamie Li <lijamie19828@gmail.com>
2024-11-08 16:13:48 -0800

    organization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// global vars
+// *Global Vars
 const currentDate: Date = new Date();
 const formattedDate: string = currentDate.toLocaleDateString(
   'en-GB',
@@ -33,10 +33,10 @@ const [formattedTime, dayPeriod] = localTime.split(' ');
 const client_id = process.env.CLIENT_ID!;
 const client_secret = process.env.CLIENT_SECRET!;
 const redirect_uri = 'http://localhost:5000/api/auth/google';
-// TODO change to use actual spreadsheet later
+// ! change to use actual spreadsheet later
 const spreadsheetId = process.env.TEST_SPREADSHEET_ID;
 
-// OAuth
+// *OAuth
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
@@ -84,7 +84,49 @@ app.post('/api/auth/google', async (req, res) => {
   }
 });
 
-// Funciton to check date of cell A2 in a provided google sheet
+// *Sheet configs
+interface SheetOption {
+  sheetId: number;
+  startRowIndex: number;
+  endRowIndex: number;
+  startColumnIndex: number;
+  endColumnIndex: number;
+}
+// range options for individual sheets
+const weightSheetOptions = {
+  sheetId: 1606005094,
+  startRowIndex: 1,
+  endRowIndex: 2,
+  startColumnIndex: 0,
+  endColumnIndex: 3,
+};
+
+const bpSheetOptions = {
+  sheetId: 2094284245,
+  AM: {
+    startRowIndex: 1,
+    endRowIndex: 2,
+    startColumnIndex: 1,
+    endColumnIndex: 6,
+  },
+  PM: {
+    startRowIndex: 1,
+    endRowIndex: 2,
+    startColumnIndex: 6,
+    endColumnIndex: 10,
+  },
+};
+
+// ! remove and replace with relevant sheetOptions later
+const testSheetOptions = {
+  sheetId: 0,
+  startRowIndex: 1,
+  endRowIndex: 2,
+  startColumnIndex: 0,
+  endColumnIndex: 3,
+};
+
+// *Functions
 const dateCheck = async (
   spreadsheetId: string | undefined,
   sheetName: string,
@@ -122,57 +164,13 @@ const dateCheck = async (
   }
 };
 
-// Sheet configs
-interface SheetOption {
-  sheetId: number;
-  startRowIndex: number;
-  endRowIndex: number;
-  startColumnIndex: number;
-  endColumnIndex: number;
-}
-// range options for individual sheets
-const weightSheetOptions = {
-  sheetId: 1606005094,
-  startRowIndex: 1,
-  endRowIndex: 2,
-  startColumnIndex: 0,
-  endColumnIndex: 3,
-};
-
-const bpSheetOptions = {
-  sheetId: 2094284245,
-  AM: {
-    startRowIndex: 1,
-    endRowIndex: 2,
-    startColumnIndex: 1,
-    endColumnIndex: 6,
-  },
-  PM: {
-    startRowIndex: 1,
-    endRowIndex: 2,
-    startColumnIndex: 6,
-    endColumnIndex: 10,
-  },
-};
-
-// TODO remove and replace with relevant sheetOptions
-const testSheetOptions = {
-  sheetId: 0,
-  startRowIndex: 1,
-  endRowIndex: 2,
-  startColumnIndex: 0,
-  endColumnIndex: 3,
-};
-
 // Function to generate a values array for sheets updateCells method
-// replaces updateCellsRequest.updateCells.rows[0].values
 const valuesFormatting = (inputs: any[]) => {
   return inputs.map((input) => ({
     userEnteredValue: { stringValue: input },
   }));
 };
 
-// currently using testSheetOptions
 // Only used with batchUpdates for the following consecutive actions insert row above, insert data
 const formatBatchUpdateRequest = async (
   inputs: any[],
@@ -213,9 +211,10 @@ const formatBatchUpdateRequest = async (
   return finalRequest;
 };
 
-// Weight
+// *Weight
 // NOTE /tracking/weight is currently using testSheetOptions so it is being written to a test document
-// TODO refactor to use weightSheetOptions
+// ! Using testSheetOptions
+// TODO Replace testSheetOptions with weightSheetOptions in weightBatchRequest
 app.post('/tracking/weight', async (req, res) => {
   const sheets = google.sheets({
     version: 'v4',
@@ -239,11 +238,11 @@ app.post('/tracking/weight', async (req, res) => {
   }
 });
 
-// Blood Pressure
+// *Blood Pressure
 
-// Meals
+// *Meals
 
-// Tests
+// *Tests
 app.post('/dateCheckTest', async (req, res) => {
   const sheets = google.sheets({
     version: 'v4',
@@ -252,7 +251,7 @@ app.post('/dateCheckTest', async (req, res) => {
   await dateCheck(spreadsheetId, 'Sheet1', sheets);
 });
 
-// Initialize sheets - Goes in each route, avoid stale state etc
+// *Initialize sheets - Goes in each route, avoid stale state etc
 // const sheets = google.sheets({
 //   version: 'v4',
 //   auth: oAuth2Client,
