0d8ad2b36e1c73f11832d3fd93855839f9341473
Jamie Li <lijamie19828@gmail.com>
2024-12-03 17:06:28 -0800

    remove force utc, causing time drifting

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -11,14 +11,11 @@ export const defaultBPInput = {
   pulse: [0, 0, 0],
 };
 
-//* Force timeZone to stop day drifting due to generating as local time
-//* and toLocaleDateString again applying local time
 export const defaultDateString = new Date().toLocaleDateString(
   'en-GB',
   {
     day: '2-digit',
     month: '2-digit',
     year: '2-digit',
-    timeZone: 'UTC',
   }
 );
